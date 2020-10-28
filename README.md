fe45902e72ac70c50bc0230af953c3d8bf4e455e
AeBiCe <Nikolaoz1997@gmail.com>
2020-10-29 00:06:20 +0100

    getting started

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# FF-magic-elixir
-A project to automize the process of making a working schedule
+# <img src="http://aebice.com/img/LOGO_Fatty's-fire-elixit.png" alt="logo" width="250"/>
+## Fatty's & Firefoot's magic elixir
+A project to automize the process of making a working schedule.
+
+
+
+
+
+
+
---
+++ program.py
@@ -0,0 +1,11 @@
+print("\n    Fatty's & Firefoot's magic elixir\n"'"Att göra scheman har aldrig var så enkelt"'"\n") # Welcome screen
+
+
+jobbare = []
+
+print("Fortsätt att ange jobbarnas namn, avsluta med 'x'")
+while ("x" not in jobbare):
+    jobbare.append(input("Namn på jobbare: "))
+
+
+print(jobbare)
